65fcb18aad005cf60ff11d106c98ce405b05472e
Anders Hafreager <andershaf@gmail.com>
2017-08-01 10:59:00 -0600

    Fixed so vsq actually calculates vsq

--- src/compute_vsq_andershaf.cpp
+++ src/compute_vsq_andershaf.cpp
@@ -37,7 +37,7 @@ ComputeVSQ::ComputeVSQ(LAMMPS *lmp, int narg, char **arg) :
 
 void ComputeVSQ::init()
 {
-  pfactor = 0.5 * force->mvv2e;
+  
 }
 
 /* ---------------------------------------------------------------------- */
@@ -53,20 +53,14 @@ double ComputeVSQ::compute_scalar()
   int *type = atom->type;
   int nlocal = atom->nlocal;
 
-  double ke = 0.0;
+  double vsq = 0.0;
 
   if (rmass) {
     for (int i = 0; i < nlocal; i++)
       if (mask[i] & groupbit)
-        ke += rmass[i] * (v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]);
-  } else {
-    for (int i = 0; i < nlocal; i++)
-      if (mask[i] & groupbit)
-        ke += mass[type[i]] *
-          (v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]);
+        vsq += (v[i][0]*v[i][0] + v[i][1]*v[i][1] + v[i][2]*v[i][2]);
   }
 
-  MPI_Allreduce(&ke,&scalar,1,MPI_DOUBLE,MPI_SUM,world);
-  scalar *= pfactor;
+  MPI_Allreduce(&vsq,&scalar,1,MPI_DOUBLE,MPI_SUM,world);
   return scalar;
 }
